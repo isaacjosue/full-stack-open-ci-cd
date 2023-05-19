22a674d6ecfa8dedc3826de3cabc2a413959c490
Isaac <isaacjosue.r.s@gmail.com>
2023-05-19 11:47:59 -0500

    Modify error for testing

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get('/version', (req, res) => {
 })
 
 app.get('/health', (req, res) => {
-  throw 'error...'
+  throw new Error('error...')
   // eslint-disable-next-line no-unreachable
   res.send('ok')
 })
